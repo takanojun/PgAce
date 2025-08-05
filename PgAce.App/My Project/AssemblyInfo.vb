01eb1edc376e7965c1eff1f5f1787a2e7ca7929e
takanojun <80045800+takanojun@users.noreply.github.com>
2025-08-05 23:50:17 +0900

    fix namespace and assembly attributes

--- PgAce.App/My Project/AssemblyInfo.vb
+++ PgAce.App/My Project/AssemblyInfo.vb
@@ -1,16 +1,7 @@
 Imports System.Reflection
 Imports System.Runtime.InteropServices
 
-<Assembly: AssemblyTitle("PgAce")>
 <Assembly: AssemblyDescription("Portable PostgreSQL client")>
-<Assembly: AssemblyConfiguration("")>
-<Assembly: AssemblyCompany("")>
-<Assembly: AssemblyProduct("PgAce")>
 <Assembly: AssemblyCopyright("Copyright (c) 2024 PgAce")>
-<Assembly: AssemblyTrademark("")>
-<Assembly: AssemblyCulture("")>
-
 <Assembly: ComVisible(False)>
 
-<Assembly: AssemblyVersion("0.1.0.0")>
-<Assembly: AssemblyFileVersion("0.1.0.0")>
